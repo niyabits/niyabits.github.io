26cece588d874c6198ee7837d553df2fd25acec6
Niya <niyabits@gmail.com>
2024-10-03 19:10:11 +0530

    feat: initial changes

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,4 +1,3 @@
-import IconDribbble from "../components/icons/dribbble.astro";
 import IconGithub from "../components/icons/github.astro";
 import IconLinkedin from "../components/icons/linkedin.astro";
 import IconTwitter from "../components/icons/twitter.astro";
@@ -6,22 +5,17 @@ import IconTwitter from "../components/icons/twitter.astro";
 export const SOCIALS = [
   {
     name: "Github",
-    url: "https://github.com/thevrus",
+    url: "https://github.com/niyabits",
     icon: IconGithub,
   },
   {
     name: "Linkedin",
-    url: "https://www.linkedin.com/in/vrusin/",
+    url: "https://www.linkedin.com/in/niyabits/",
     icon: IconLinkedin,
   },
   {
     name: "Twitter",
-    url: "https://twitter.com/rusin_vadim",
+    url: "https://twitter.com/niyabits",
     icon: IconTwitter,
-  },
-  {
-    name: "Dribbble",
-    url: "https://dribbble.com/iamvr",
-    icon: IconDribbble,
   },
 ];
